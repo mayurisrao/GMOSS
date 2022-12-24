0806d1761104830ec49a0e12e4f61fb6b64a2076
mohithpa <mohithpa2@gmail.com>
2022-12-24 12:25:52 +0530

    added GMOSS

---
+++ GMOSS.py
@@ -0,0 +1,79 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sp
+import time as time
+import glob
+
+start = time.time()
+pixels = 3072
+
+#some important constants
+kk = 1.3806488e-23 # m^2 kg s^-1
+hh = 6.62606957e-34 # m^2 kg s^-2 K^-1
+cvel = 2.99792458e+08 # m s^-1
+m_e = 9.1e-31
+q_e = 1.6e-19
+sin_alph = 1.0
+Bmag = 1e-9 # Tesla == 10 micro-Gauss
+#PI = 3.14159265358979
+FTOL = 1.0e-6
+T_e = 8000.0 # Thermal electron temperature
+TCMB = 2.72548
+NHPIX = 3072
+GSPAN = 100.0
+
+
+#apply corrections to 150,408,1420
+corrections_applied_to = np.array([150,408,1420])
+
+#some important stuff
+scale_gam_nu = (3.0*q_e*Bmag*sin_alph)/(4.0*np.pi*m_e*cvel)
+
+#appending the maps to the list
+text_files = glob.glob("*.txt")
+map_files = []
+for file in text_files:
+    if file.startswith("map"):
+        map_files.append(file)
+
+frequency = np.array([])
+
+#obtaining the sorted list of maps and frequencies
+for file in map_files:
+    frequency = np.append(frequency, float(file.split("_")[1]))
+
+frequency = np.sort(frequency)
+frequency = [int(i) for i in frequency]
+
+map_files = [f'map_{i}_r4_5deg_nested_galactic_Kelvin.txt' for i in frequency]
+
+#obtaining frequency in GHz and the log of the frequencyies
+frequency_in_GHz = [np.float32(f*10**-3) for f in frequency] 
+#print(frequency)
+
+f = open("convexity.txt", "w")
+
+######################Starting our main program#####################
+for pixel in range(pixels):
+    print(f"Pixel Number: {pixel+1}")
+    b_temp = np.array([])
+    for file in map_files:
+        data = np.genfromtxt(file)
+        b_temp= np.append(b_temp, data[pixel])
+
+    #to obtain concavity or convexity
+    #Spectral index between 45 MHz and 150 MHz
+    alpha1 = (np.log10(b_temp[1]) - np.log10(b_temp[2]))/(np.log10(frequency_in_GHz[2]) - np.log10(frequency_in_GHz[1]))
+    if alpha1 < 2.0: alpha1 = 2.
+    if alpha1 > 3.0: alpha1 = 3.
+    
+    #to obtain concave or convexity
+    #Spectral index between 408 MHz and 1420 MHz
+    alpha2 = (np.log10(b_temp[3]) - np.log10(b_temp[4]))/(np.log10(frequency_in_GHz[4]) - np.log10(frequency_in_GHz[3]))
+    if alpha2 < 2.0: alpha2 = 2.
+    if alpha2 > 3.0: alpha2 = 3.
+
+    if alpha1 < alpha2: f.write(f"{pixel+1} convex\n")
+    elif alpha1 > alpha2: f.write(f"{pixel+1} concave\n")
+
+f.close()
--- scipy_optimize_next_try.py
+++ scipy_optimize_next_try.py
@@ -5,7 +5,8 @@ from scipy.optimize import minimize
 #The docstring lines below are working piece of code.
 
 frequency = np.array([22,45,150,408,1420,23000]) #x_values
-b_temp = np.array([5.49282937e+04, 1.04875149e+04, 5.57289208e+02, 4.39957717e+01, 4.37429422e+00, 1.07386277e-03]) #y_values
+#b_temp = np.array([5.49282937e+04, 1.04875149e+04, 5.57289208e+02, 4.39957717e+01, 4.37429422e+00, 1.07386277e-03]) #y_values
+b_temp = [2.55080863e+04, 4.90777800e+03, 2.28984753e+02, 2.10842949e+01, 3.58631166e+00, 5.68716056e-04]
 
 #Defining the function that I want to fit
 def concave_func(x, C_1, C_2, alpha_one, alpha_two, I_x, nu_t, T_e): 
@@ -25,16 +26,24 @@ def chisq(params, xobs, yobs):
     print(yerr)
     return yerr
 
-result = minimize(chisq, [1,2,1,1,1,1,1], args = (frequency,b_temp),  method = 'Nelder-Mead', options = {'disp' : True, 'maxiter': 10000})
+result = minimize(chisq, [1,2,2,2,1,1e6,8000], args = (frequency,b_temp),  method = 'Nelder-Mead', options = {'disp' : True, 'maxiter': 10000})
+#result = minimize(chisq, [1,2,1,1,1,1,1], args = (frequency,b_temp),  method = 'Nelder-Mead', options = {'disp' : True, 'maxiter': 10000})
 #ynw3 = concave_func(frequency, *result.x)
 x = np.linspace(-300,24000,1000)
+plt.yscale("log")
+plt.xscale("log")
 plt.plot(x,concave_func(x, *result.x) )
 print(result.x)
 print(result)
 #plt.plot(frequency, ynw3)
-plt.plot(frequency, b_temp, 'r*')
+plt.plot(frequency, b_temp, 'r*' )
+plt.xlabel("log Frequency[MHz]")
+plt.ylabel("log Temp[K]")
+plt.title('log Temparature vs log Frequency')
+
 plt.grid()
-plt.show()
+plt.savefig('the_plot_2060.png')
+
 
 '''
 frequency = np.array([22,45,150,408,1420,23000]) #x_values
@@ -58,13 +67,18 @@ def chisq(params, xobs, yobs):
     print(yerr)
     return yerr
 
-result = minimize(chisq, [2,2,1,1,1,1,1], args = (log_frequency,log_b_temp), method = 'Nelder-Mead', options = {'disp' : True, 'maxiter': 10000})
+result = minimize(chisq, [2,2,1,1,1,1,1], args = (frequency,b_temp), method = 'Nelder-Mead', options = {'disp' : True, 'maxiter': 10000})
 #ynw3 = concave_func(log_frequency, *result.x)
 print(result.x)
-x = np.linspace(min(log_frequency),max(log_frequency),1000)
-plt.plot(x,concave_func(x, *result.x) )
+x = np.linspace(10,24000,10)
+plt.plot(x,concave_func(x, *result.x))
 #plt.plot(log_frequency, ynw3)
-plt.plot(log_frequency, log_b_temp, 'r*')
+plt.plot(frequency, b_temp, 'r*')
 print(result)
+#plt.yscale("log")
+#plt.xscale("log")
+#print(x)
+#print(concave_func(frequency, *result.x))
 plt.show()
+
 '''
